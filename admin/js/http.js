832500eabed5b3b75d4fed13115f10d4737e016e
limeQuery <hgf17620017680@163.com>
2019-12-29 15:08:53 +0800

    整体页面渲染完成12.29.1

--- admin/js/http.js
+++ admin/js/http.js
@@ -33,14 +33,16 @@
         {
             // 发送ajax之前执行的函数
             beforeSend(xhr) {
-                //判断 如果没有令牌就打回登录页面
-                if (!token) {
-                    location.href = './login.html';
-                }
                 // 如果当前页面的不是登录页面
                 if (location.href.indexOf('login.html') === -1) {
+                    //判断 如果没有令牌就打回登录页面
+                    if (!token) {
+                        location.href = './login.html';
+                        return;
+                    }
                     //给所有的请求同意设置token请求头
                     xhr.setRequestHeader('Authorization', token)
+
                 }
             }
         }
